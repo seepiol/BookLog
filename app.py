25ac7bab07c809b09b4e0526fee67ef32d9876db
seepiol <seepiol@pm.me>
2020-01-19 18:40:18 +0100

    Python files correctly formatted with black

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
-# GEN HACKATON 2020 
+# GEN HACKATON 2020
 # 9:30PM - 3:09AM
 # PROJECT: BOOKLOG
 
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import sqlite3
+
 app = Flask(__name__)
 
 conn = sqlite3.connect("books.db", check_same_thread=False)
 try:
-    conn.execute('''CREATE TABLE BOOKS
+    conn.execute(
+        """CREATE TABLE BOOKS
                 (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                 TITLE   TEXT    NOT NULL,
                 AUTHOR  TEXT    NOT NULL,
                 PAGES   INT     NOT NULL,
-                STATUS  TEXT    NOT NULL);''')
+                STATUS  TEXT    NOT NULL);"""
+    )
 except:
     pass
+
 
 @app.route("/bookentry")
 def home():
     return render_template("entry.html")
+
 
 @app.route("/")
 @app.route("/home")
@@ -32,8 +37,8 @@ def viewbooks():
         book = [row[0], row[1], row[2], row[3], row[4]]
         bookList.append(book)
 
-    return render_template("viewbooks.html", bookList = bookList)
-    
+    return render_template("viewbooks.html", bookList=bookList)
+
 
 @app.route("/addbook", methods=["POST"])
 def addBook():
@@ -44,28 +49,32 @@ def addBook():
     if not bookTitle or not bookAuthor or not bookPages:
         return render_template("fail.html")
     else:
-        conn.execute(f'INSERT INTO BOOKS (TITLE, AUTHOR, PAGES, STATUS)VALUES ("{bookTitle}", "{bookAuthor}", "{bookPages}", "True");')
+        conn.execute(
+            f'INSERT INTO BOOKS (TITLE, AUTHOR, PAGES, STATUS)VALUES ("{bookTitle}", "{bookAuthor}", "{bookPages}", "True");'
+        )
         conn.commit()
         return render_template("success.html")
 
 
-@app.route("/setbookstatus", methods = ['POST', 'GET'])
+@app.route("/setbookstatus", methods=["POST", "GET"])
 def setBookStatus():
-    bookId = request.args.get('bookid')
-    currentStatus = request.args.get('currentstatus')
+    bookId = request.args.get("bookid")
+    currentStatus = request.args.get("currentstatus")
     if currentStatus == "True":
-        conn.execute(f"UPDATE BOOKS SET STATUS = \"False\" WHERE ID = {bookId}")
+        conn.execute(f'UPDATE BOOKS SET STATUS = "False" WHERE ID = {bookId}')
     elif currentStatus == "False":
-        conn.execute(f"UPDATE BOOKS SET STATUS = \"True\" WHERE ID = {bookId}")
+        conn.execute(f'UPDATE BOOKS SET STATUS = "True" WHERE ID = {bookId}')
     return redirect("/viewbooks")
-    
-@app.route("/deletebook", methods = ["GET"])
+
+
+@app.route("/deletebook", methods=["GET"])
 def deleteBook():
     bookId = request.args.get("bookid")
     conn.execute(f"DELETE FROM BOOKS WHERE ID = {bookId}")
     conn.commit()
     return redirect("/viewbooks")
 
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host = "0.0.0.0", port = 4000)
+    app.run(host="0.0.0.0", port=4000)
